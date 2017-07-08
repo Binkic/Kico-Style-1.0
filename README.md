595372be0b6e00e84d749dbeb4d932f2c860e494
奇趣保罗 <dreamer_paul@126.com>
2017-07-09 01:29:02 +0800

    First Commit
    初版 Kico Style 上线啦~

---
+++ README.md
@@ -0,0 +1,23 @@
+﻿# Kico Style
+一个简洁、有趣的开源响应式框架，仅包含基础样式，需按照一定规则进行二次开发。
+
+## 使用方法
+1. Star 本项目
+2. Clone 本项目
+3. 参照开发文档（本项目已内置）来进行食用即可~
+
+## 项目故事
+在开发完我的第一个门户网站（缤奇首页）项目后，发现其中的底层重置样式都是可以直接复制到新项目的，于是顿时想到了“框架”这个概念。想创立一个框架统一下团队的代码风格，因此出现了最早的“binkic.css”...
+
+如有兴趣的话欢迎点击 [这里](https://hi-paul.space/kico-style-story.html) 详细阅读~
+
+## 开源协议
+本项目采用 MIT 开源协议进行授权，并在其基础上须保留原作者的版权注释（CSS、JS 等文件），当然能在页尾写上我们的框架就是最好的。
+
+如支持我的话请 Star 本项目或前往 [我的博客](https://hi-paul.space) 给予赞助。
+
+## 参考过的开源项目
+ - [Bootstrap](https://github.com/twbs/bootstrap)
+
+## 开发文档内使用的开源项目
+ - [Font Awesome](https://github.com/FortAwesome/Font-Awesome)
---
+++ js/docs.js
@@ -0,0 +1,57 @@
+/* ----
+
+# Kico Style Docs
+# By: Dreamer-Paul
+# Last Update: 2017.7.9
+
+一个简洁、有趣的开源响应式框架，仅包含基础样式，需按照一定规则进行二次开发。
+
+欢迎你加入缤奇，和我们一起改变世界。
+本代码为缤奇保罗原创，并遵守 MIT 开源协议。保罗的个人博客：https://hi-paul.space
+
+---- */
+
+// 菜单按钮
+function nav_btn() {
+    var nav_btn = document.getElementsByClassName('toggle-btn')[0];
+    var nav_bar = document.getElementsByClassName('head-menu')[0];
+
+    nav_btn.addEventListener("click", function () {
+        nav_bar.classList.toggle("active");
+    });
+}
+nav_btn();
+
+function navtrans() {
+    var header = document.getElementsByTagName("header")[0];
+    var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+    if(scroll >= 160)
+    {
+        header.classList.add("active");
+    }
+    else
+    {
+        header.classList.remove("active");
+    }
+}
+
+// 遮罩示例
+function overlay_test_a() {
+    overlay(3000);
+}
+
+function alert_test_a() {
+    alert_box("这是一个弹窗测试", 1000);
+}
+function alert_test_b() {
+    alert_box("这是一个弹窗测试", 1000, false);
+}
+function alert_test_c() {
+    alert_box("这是一个弹窗测试", 1000, true, "green");
+}
+
+// 页面滚动事件
+window.onscroll = function () {
+    navtrans();
+};
